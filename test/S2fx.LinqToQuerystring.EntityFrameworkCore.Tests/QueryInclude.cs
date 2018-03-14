98b725523e3eaf6d44be156e7a754863b343913c
Wei "oldrev" Li <liwei@sandwych.com>
2018-03-14 23:37:42 +0800

    refactor and add support of EFCore

---
+++ test/S2fx.LinqToQuerystring.EntityFrameworkCore.Tests/QueryInclude.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LinqToQuerystring.EntityFrameworkCore.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using LinqToQuerystring;
+    using Machine.Specifications;
+
+    public class QueryInclude
+    {
+        Establish context = () =>
+        {
+            Value = 1;
+        };
+
+        public class Test1
+        {
+            private Because of = () => Value += 1;
+            private It should_return_three_results = () => Value.ShouldBeGreaterThan(1);
+        }
+
+        static int Value;
+    }
+}
