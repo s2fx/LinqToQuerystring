10e107d174b551ece88596b1d3a0c0b34fa7fb09
Wei "oldrev" Li <liwei@sandwych.com>
2018-03-15 00:58:44 +0800

    add unit test for efcore

---
+++ test/S2fx.LinqToQuerystring.EntityFrameworkCore.Tests/Exceptions.cs
@@ -0,0 +1,44 @@
+﻿namespace LinqToQueryString.EntityFrameworkCore.Tests
+{
+    using System;
+    using System.Data;
+    using System.Linq;
+
+    using LinqToQuerystring;
+
+    using Machine.Specifications;
+    using Microsoft.EntityFrameworkCore;
+
+    public class When_using_skip_on_unordered_data : SqlPagingAndOrdering
+    {
+        private static Exception ex;
+
+        private Because of = () => ex = Catch.Exception(() => result = testDb.ConcreteCollection.LinqToQuerystring("?$skip=1").ToList());
+
+        private It should_throw_an_exception = () => ex.ShouldBeOfExactType<NotSupportedException>();
+    }
+
+    public class When_trying_to_order_by_complex_types : SqlPagingAndOrdering
+    {
+        private static Exception ex;
+
+        private Because of = () => ex = Catch.Exception(() => complexResult = testDb.ComplexCollection.LinqToQuerystring("?$orderby=concrete").ToList());
+
+        private It should_throw_an_exception = () => ex.ShouldBeOfExactType<ArgumentException>();
+    }
+
+    /*
+    public class When_filtering_on_endswith_function : SqlFunctions
+    {
+        private static Exception ex;
+
+        private Because of = () => ex = Catch.Exception(() => testDb.ConcreteCollection.LinqToQuerystring("?$filter=endswith(Name,'day')").ToList());
+
+        private It should_throw_an_exception = () => ex.ShouldBeOfExactType<EntityCommandCompilationException>();
+
+        private It should_fail_due_to_SQL_CE_not_supporting_endswith =
+            () =>
+            ex.InnerException.Message.ShouldEqual("The function 'Reverse' is not supported by SQL Server Compact.");
+    }
+    */
+}
---
+++ test/S2fx.LinqToQuerystring.EntityFrameworkCore.Tests/TestDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace LinqToQueryString.EntityFrameworkCore.Tests
+{
+    using LinqToQueryString.Tests;
+    using Microsoft.EntityFrameworkCore;
+
+    public class TestDbContext : DbContext
+    {
+        public DbSet<ConcreteClass> ConcreteCollection { get; set; }
+
+        public DbSet<ComplexClass> ComplexCollection { get; set; }
+
+        public DbSet<EdgeCaseClass> EdgeCaseCollection { get; set; }
+
+        public DbSet<NullableClass> NullableCollection { get; set; }
+
+        public DbSet<NullableContainer> NullableContainers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=TestDb.sqlite");
+        }
+    }
+}
